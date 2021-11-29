e32c293ce38084abc4c5b392cc9fad14b7a989d9
Justin Houk <jstndhouk@gmail.com>
2021-11-29 15:52:13 -0500

    added three models

---
+++ models/Pet.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes, NOW } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Pet extends Model {}
+
+Pet.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    pet_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    pet_type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    pet_species: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    pet_owner_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: 
+      {
+        model: 'pet',
+        key: 'id',
+      }
+    },
+    boarded: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+    },
+    check_in_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    check_out_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    duration_of_stay: {
+      type: DataTypes.INTEGER,
+      defaultValue: false,
+      allowNull: false
+    },
+    kennel_size: {
+      type: DataTypes.STRING(1),
+      defaultValue: false,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'pet',
+  }
+);
+
+module.exports = Pet;
---
+++ models/ScheduledActivity.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class ScheduledActivity extends Model {}
+
+ScheduledActivity.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    activity_type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    activity_description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    pet_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: 
+      {
+        model: 'pet',
+        key: 'id',
+      },
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'scheduledActivity',
+  }
+);
+
+module.exports = ScheduledActivity;
