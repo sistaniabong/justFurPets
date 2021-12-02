75c5e5b5b51aa34851da1b3729becdcafa99d88a
Justin Houk <jstndhouk@gmail.com>
2021-12-01 19:07:51 -0500

    seeds work properly

--- seeds/index.js
+++ seeds/index.js
@@ -6,23 +6,31 @@ const petOwnerData = require('./petOwnerData.json');
 const scheduledActivityData = require('./scheduledActivityData.json');
 
 const seedAll = async () => {
+  try{
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-
-  const pets = await Pet.bulkCreate(petData, {
-    individualHooks: true,
-    returning: true,
-  });
 
   const petOwners = await PetOwner.bulkCreate(petOwnerData, {
     individualHooks: true,
     returning: true,
   });
 
-  const scheduledActivities = await ScheduledActivity.bulkCreate(scheduledActivityData, {
+  const pets = await Pet.bulkCreate(petData, {
     individualHooks: true,
     returning: true,
   });
+  const scheduledActivities = await ScheduledActivity.bulkCreate(scheduledActivityData, {
+    individualHooks: true,
+    returning: true,
+  });  
+
+}
+  catch(error){
+    console.log(error)
+  }
+  
+
+ 
 
   process.exit(0);
 };
