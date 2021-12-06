e4d5bf0a052f58e9eaa4fcd5def4663f6224d586
Vince Momot <vincemomot@gmail.com>
2021-12-05 20:40:44 -0500

    halfway through profile pets page

--- routes/api/petRoute.js
+++ routes/api/petRoute.js
@@ -15,31 +15,30 @@ router.get('/', async (req, res) => {
         include: [
           {
             model: PetOwner,
-            attributes: ['owner_name','phone_number'],
+            attributes: ['owner_name', 'phone_number'],
           },
         ],
       });
       // res.status(200).json(petsData);
       const pets = petsData.map((pet) => pet.get({ plain: true }));
-      res.render('allpets', { 
-        pets,
-        
-        logged_in: req.session.logged_in 
-      },);
-    }else{
+      res.render('allpets', {
+        pets
+        // logged_in: req.session.logged_in 
+      });
+    } else {
       const petsData = await Pet.findAll({
         include: [
           {
             model: PetOwner,
-            attributes: ['owner_name','phone_number'],
+            attributes: ['owner_name', 'phone_number'],
           },
         ],
       });
 
       const pets = petsData.map((pet) => pet.get({ plain: true }));
-      res.render('allpets', { 
+      res.render('allpets', {
         pets,
-        logged_in: req.session.logged_in 
+         logged_in: req.session.logged_in 
       });
     }
   } catch (err) {
@@ -55,58 +54,59 @@ router.get('/:id', async (req, res) => {
   // find all pets
   try {
     const petData = await Pet.findByPk(req.params.id);
-    res.status(200).json(petData);              
+    res.status(200).json(petData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.post('/', async (req, res) => {
-    // create a new pet
-    try {
-      const petData = await Pet.create(req.body);
-      res.status(200).json(petData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+  // create a new pet
+  try {
+    const petData = await Pet.create(req.body);
+    res.status(200).json(petData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 router.put('/:id', async (req, res) => {
-    // update a tag's name by its `id` value
-    try {
-      const petData = await Pet.update(
-        {
-          pet_name: req.body.pet_name,
-          pet_type: req.body.pet_type,
-          pet_species: req.body.pet_species,
-          pet_owner_id: req.body.pet_owner_id,
-          boarded: req.body.boarded,
-          check_in_date: req.body.check_in_date,
-          check_out_date: req.body.check_out_date,
-          stay_duration: req.body.stay_duration,
-          kennel_size: req.body.kennel_size
-        },
+  // update a tag's name by its `id` value
+  try {
+    const petData = await Pet.update(
+      {
+        pet_name: req.body.pet_name,
+        pet_type: req.body.pet_type,
+        pet_species: req.body.pet_species,
+        pet_owner_id: req.body.pet_owner_id,
+        boarded: req.body.boarded,
+        check_in_date: req.body.check_in_date,
+        check_out_date: req.body.check_out_date,
+        stay_duration: req.body.stay_duration,
+        kennel_size: req.body.kennel_size
+      },
 
-        {where: {
+      {
+        where: {
           id: req.params.id
         }
       });
-  
-      if (!petData) {
-        res.status(404).json({ message: 'No pet found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(petData);
-    } catch (err) {
-      res.status(500).json(err);
+
+    if (!petData) {
+      res.status(404).json({ message: 'No pet found with this id!' });
+      return;
     }
-  });
+
+    res.status(200).json(petData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  try{
+  try {
     Pet.destroy({
       where: {
         id: req.params.id,
