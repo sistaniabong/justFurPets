65d28db826443d25d102846abf0a4af2c9754715
Justin Houk <jstndhouk@gmail.com>
2021-12-07 20:43:22 -0500

    Update petActivity.js

--- routes/api/petActivity.js
+++ routes/api/petActivity.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { ScheduledActivity } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth async (req, res) => {
     try {
         const activityData = await ScheduledActivity.create(req.body);
         console.log(activityData);
