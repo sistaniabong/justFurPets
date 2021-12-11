11ca1ee23a468d0b8e37d6c21702eafad5ff9b14
Sistania Bong <sistaniameilina@gmail.com>
2021-12-10 19:41:32 -0500

    fix date display in edit pet page

--- utils/helper.js
+++ utils/helper.js
@@ -2,6 +2,18 @@ module.exports = {
   
     format_date: (date) => {
       return date.toLocaleDateString();
-
     },
+    format_date_value_display: (date) => {
+      let month = date.getMonth() + 1;
+      let day = date.getDate();
+      let year = date.getFullYear();
+      if(day<=9) {
+        day = '0' + day
+      }
+      if(month<10) {
+        month = '0' + month
+      }
+      const postDate = year + '-' + month + '-' + day
+      return postDate
+    }
 };
