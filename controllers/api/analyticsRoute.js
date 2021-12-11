1c22961c60f44e55e3232a4951cb0f70c641dbc7
Vince Momot <vincemomot@gmail.com>
2021-12-10 23:22:55 -0500

    added the days until leaving in the analytics

--- controllers/api/analyticsRoute.js
+++ controllers/api/analyticsRoute.js
@@ -1,76 +1,83 @@
 const router = require('express').Router();
+const { NOW } = require('sequelize/dist');
 const sequelize = require('../../config/connection');
 const { Pet, PetOwner, User, ScheduledActivity } = require('../../models');
+const { max } = require('../../models/Pet');
 const withAuth = require('../../utils/auth');
 
 
-router.get('/', withAuth, async(req,res) => {
+
+router.get('/', withAuth, async (req, res) => {
   try {
-    const petsData = await Pet.findAll({  	
+    const petsData = await Pet.findAll({
       attributes: ['pet_type', [sequelize.fn('count', sequelize.col('pet_type')), 'count']],
-      group : ['pet.pet_type'],
+      group: ['pet.pet_type'],
       raw: true,
       order: [['pet_type', 'ASC']]
     });
 
     const pet_counts = petsData.map((pet) => pet.count);
 
-    const totalPetData = await Pet.findAll({  	
+    const totalPetData = await Pet.findAll({
       attributes: [[sequelize.fn('count', sequelize.col('id')), 'total_count']],
       raw: true,
     });
+    ///////////////////TOP
+    const allDates = await Pet.findAll({
+      attributes: [[sequelize.fn('max', sequelize.col('check_out_date')), 'max_date']],
+      raw: true,
+    });
 
-    const totalPetActivity = await ScheduledActivity.findAll({  	
+    let today = new Date();
+
+    var Difference_In_Time = (allDates[0].max_date).getTime() - today.getTime();
+
+
+    var Difference_In_Days = Math.floor(Difference_In_Time / (1000 * 3600 * 24));
+  
+
+    /////////BOT
+    const totalPetActivity = await ScheduledActivity.findAll({
       attributes: [[sequelize.fn('count', sequelize.col('id')), 'total_activity']],
       raw: true,
     });
 
 
-    //Vince's 
-    // const petDates = await Pet.findAll({
-    //   attributes: [[sequelize.fn('date', sequelize.col("id")), 'check_out_date' ]],
-    //   raw:true,
-    // });
-
-    // console.log(petDates);
-    // console.log(totalPetData);
-
-////end
 
 
-    const activityData = await Pet.findAll({  	
+    const activityData = await Pet.findAll({
       include: [
         {
           model: ScheduledActivity,
           attributes: ['activity_description'],
         },
-      ],     
+      ],
     });
 
     const petActivities = activityData.map((activity) => activity.get({ plain: true }));
     let petsWithNoActivities = [];
-    for(let i = 0; i < petActivities.length; i++)
-    {
-      if(petActivities[i].scheduledActivities == "")
-      {
+    for (let i = 0; i < petActivities.length; i++) {
+      if (petActivities[i].scheduledActivities == "") {
         petsWithNoActivities.push(petActivities[i].pet_name)
       }
     }
-    let activityNumbers = [petActivities.length+1 - petsWithNoActivities.length, petsWithNoActivities.length+1 ]
+    let activityNumbers = [petActivities.length + 1 - petsWithNoActivities.length, petsWithNoActivities.length + 1]
     res.render('analytics',
-    {
-      total_pet:totalPetData[0].total_count,
-      total_activity:totalPetActivity[0].total_activity,
-      pet_count:pet_counts,
-      logged_in: req.session.logged_in,
-      petsWithNoActivities,
-      activityNumbers
-      
-    });
-  } catch(err){
+      {
+        total_pet: totalPetData[0].total_count,
+        total_activity: totalPetActivity[0].total_activity,
+        pet_count: pet_counts,
+        max_stay: Difference_In_Days,
+        logged_in: req.session.logged_in,
+        petsWithNoActivities,
+        activityNumbers
+
+      });
+  } catch (err) {
     console.log(err)
   }
-    
-  });
 
-module.exports=router;
+});
+
+
+module.exports = router;
