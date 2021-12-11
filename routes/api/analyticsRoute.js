048137c5a9aa74f29bfd460aa52070fe7bd5f2d3
Justin Houk <jstndhouk@gmail.com>
2021-12-10 19:40:35 -0500

    fixed conflict

--- routes/api/analyticsRoute.js
+++ routes/api/analyticsRoute.js
@@ -19,19 +19,56 @@ router.get('/', withAuth, async(req,res) => {
       attributes: [[sequelize.fn('count', sequelize.col('id')), 'total_count']],
       raw: true,
     });
-    console.log(totalPetData[0].total_count)
+    // console.log(totalPetData[0].total_count)
 
     const totalPetActivity = await ScheduledActivity.findAll({  	
       attributes: [[sequelize.fn('count', sequelize.col('id')), 'total_activity']],
       raw: true,
     });
 
+    const activityData = await Pet.findAll({  	
+      include: [
+        {
+          model: ScheduledActivity,
+          attributes: ['activity_description'],
+        },
+      ],     
+    });
+
+
+    const petActivities = activityData.map((activity) => activity.get({ plain: true }));
+    let petsWithNoActivities = [];
+    let petsActivitiesNum=0;
+    let petWithMostActivities=[];
+    for(let i = 0; i < petActivities.length; i++)
+    {
+      if(petActivities[i].scheduledActivities == "")
+      {
+        petsWithNoActivities.push(petActivities[i].pet_name)
+        
+      }
+      if(petActivities[i].scheduledActivities.length > petsActivitiesNum){
+        petsActivitiesNum=petActivities[i].scheduledActivities.length;
+        petWithMostActivities=petActivities[i];
+      }
+      
+     
+    }
+    let busiestPet= petWithMostActivities.pet_name;
+
+
+    let activityNumbers = [petActivities.length - petsWithNoActivities.length, petsWithNoActivities.length ]
+
     res.render('analytics',
     {
       total_pet:totalPetData[0].total_count,
       total_activity:totalPetActivity[0].total_activity,
       pet_count:pet_counts,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
+      logged_in: req.session.logged_in,
+      petsWithNoActivities,
+      activityNumbers,
+      busiestPet
     });
   } catch(err){
     console.log(err)
